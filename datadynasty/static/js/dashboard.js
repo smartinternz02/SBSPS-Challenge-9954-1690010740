ff8b929a16f4755d4c88904e942ba79a31886894
JENSON06 <arokyajenson@gmail.com>
2023-08-24 00:01:56 +0530

    i changes in many files

---
+++ datadynasty/static/js/dashboard.js
@@ -0,0 +1,246 @@
+var explorationTopbox = document.getElementById("explorationTopbox");
+if(explorationTopbox) {
+    explorationTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='explorationHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var explorationTopText = document.getElementById("explorationTopText");
+if(explorationTopText) {
+    explorationTopText.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='eXPLORATIONHeadText']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var hOMEText = document.getElementById("hOMEText");
+if(hOMEText) {
+    hOMEText.addEventListener("click", function (e) {
+        // Add your code here
+    });
+}
+
+var aBOUTText = document.getElementById("aBOUTText");
+if(aBOUTText) {
+    aBOUTText.addEventListener("click", function (e) {
+        // Add your code here
+    });
+}
+
+var sTORYText = document.getElementById("sTORYText");
+if(sTORYText) {
+    sTORYText.addEventListener("click", function (e) {
+        // Add your code here
+    });
+}
+
+var cONTACTText = document.getElementById("cONTACTText");
+if(cONTACTText) {
+    cONTACTText.addEventListener("click", function (e) {
+        // Add your code here
+    });
+}
+
+var phTopbox = document.getElementById("phTopbox");
+if(phTopbox) {
+    phTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='phBox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var currentstateTopbox = document.getElementById("currentstateTopbox");
+if(currentstateTopbox) {
+    currentstateTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='currentStateHeadText']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var foodsecurityTopbox = document.getElementById("foodsecurityTopbox");
+if(foodsecurityTopbox) {
+    foodsecurityTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='foodSecurityHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var rainfallTopbox = document.getElementById("rainfallTopbox");
+if(rainfallTopbox) {
+    rainfallTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='rainfallHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var cropdamageTopbox = document.getElementById("cropdamageTopbox");
+if(cropdamageTopbox) {
+    cropdamageTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='cropDamageHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var chemicalTopbox = document.getElementById("chemicalTopbox");
+if(chemicalTopbox) {
+    chemicalTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='chemicalHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var overallTopbox = document.getElementById("overallTopbox");
+if(overallTopbox) {
+    overallTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='overallHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var variableTopbox = document.getElementById("variableTopbox");
+if(variableTopbox) {
+    variableTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='varaiableHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var productionTopbox = document.getElementById("productionTopbox");
+if(productionTopbox) {
+    productionTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='populationHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var cropTopbox = document.getElementById("cropTopbox");
+if(cropTopbox) {
+    cropTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='cropProductionHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var populationTopbox = document.getElementById("populationTopbox");
+if(populationTopbox) {
+    populationTopbox.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='populationHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var rainfallTopText = document.getElementById("rainfallTopText");
+if(rainfallTopText) {
+    rainfallTopText.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='rainfallHeadText']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var populationTopText = document.getElementById("populationTopText");
+if(populationTopText) {
+    populationTopText.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='populationHeadText']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var overallTopText = document.getElementById("overallTopText");
+if(overallTopText) {
+    overallTopText.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='overallHeadText']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var variableTopText = document.getElementById("variableTopText");
+if(variableTopText) {
+    variableTopText.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='variableHeadText']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var phTopText = document.getElementById("phTopText");
+if(phTopText) {
+    phTopText.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='phHeadText']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var currentstateTopText = document.getElementById("currentstateTopText");
+if(currentstateTopText) {
+    currentstateTopText.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='currentStateHeadbox']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var cropTopText = document.getElementById("cropTopText");
+if(cropTopText) {
+    cropTopText.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='cropProductionHeadText']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+
+var chemicalTopText = document.getElementById("chemicalTopText");
+if(chemicalTopText) {
+    chemicalTopText.addEventListener("click", function () {
+        var anchor = document.querySelector("[data-scroll-to='chemicalHeadText']");
+        if(anchor) {
+            anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+    });
+}
+
+var arrowHotspot1Image = document.getElementById("arrowHotspot1Image");
+if (arrowHotspot1Image) {
+  arrowHotspot1Image.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='topGreen']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
---
+++ datadynasty/static/js/story.js
@@ -0,0 +1,177 @@
+
+    		var hOMEText = document.getElementById("hOMEText");
+    		if(hOMEText) {
+      			hOMEText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var aBOUTText = document.getElementById("aBOUTText");
+    		if(aBOUTText) {
+      			aBOUTText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var dASHBOARDText = document.getElementById("dASHBOARDText");
+    		if(dASHBOARDText) {
+      			dASHBOARDText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var cONTACTText = document.getElementById("cONTACTText");
+    		if(cONTACTText) {
+      			cONTACTText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var startContainer = document.getElementById("startContainer");
+    		if(startContainer) {
+      			startContainer.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='aLLCROPSSTORY']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var allcropsStoryTopbox = document.getElementById("allcropsStoryTopbox");
+    		if(allcropsStoryTopbox) {
+      			allcropsStoryTopbox.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='allcropsStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var overallReportTopbox = document.getElementById("overallReportTopbox");
+    		if(overallReportTopbox) {
+      			overallReportTopbox.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='overallStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var chemicalStoryTopbox = document.getElementById("chemicalStoryTopbox");
+    		if(chemicalStoryTopbox) {
+      			chemicalStoryTopbox.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='chemicalStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var productionStoryTopbox = document.getElementById("productionStoryTopbox");
+    		if(productionStoryTopbox) {
+      			productionStoryTopbox.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='productionStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var populationStoryTopbox = document.getElementById("populationStoryTopbox");
+    		if(populationStoryTopbox) {
+      			populationStoryTopbox.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='populationStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var cropsStoryTopbox = document.getElementById("cropsStoryTopbox");
+    		if(cropsStoryTopbox) {
+      			cropsStoryTopbox.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='cropStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var climateStoryTopbox = document.getElementById("climateStoryTopbox");
+    		if(climateStoryTopbox) {
+      			climateStoryTopbox.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='climateStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var allcropsStorytopText = document.getElementById("allcropsStorytopText");
+    		if(allcropsStorytopText) {
+      			allcropsStorytopText.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='allcropsStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var overallStorytopText = document.getElementById("overallStorytopText");
+    		if(overallStorytopText) {
+      			overallStorytopText.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='overallStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var chemicalStorytopText = document.getElementById("chemicalStorytopText");
+    		if(chemicalStorytopText) {
+      			chemicalStorytopText.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='chemicalStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var productionStorytopText = document.getElementById("productionStorytopText");
+    		if(productionStorytopText) {
+      			productionStorytopText.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='productionStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var cropsStorytopText = document.getElementById("cropsStorytopText");
+    		if(cropsStorytopText) {
+      			cropsStorytopText.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='cropStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var populationStorytopText = document.getElementById("populationStorytopText");
+    		if(populationStorytopText) {
+      			populationStorytopText.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='populationStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
+    		
+    		
+    		var climateStorytopText = document.getElementById("climateStorytopText");
+    		if(climateStorytopText) {
+      			climateStorytopText.addEventListener("click", function () {
+        				var anchor = document.querySelector("[data-scroll-to='climateStoryHeadbox']");
+        				if(anchor) {
+          					anchor.scrollIntoView({"block":"start","behavior":"smooth"}) }
+      			});
+    		}
